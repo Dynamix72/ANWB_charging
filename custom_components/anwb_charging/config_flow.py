2d56c19f8487a19ccf1c95b83ecad4cf5dd58478
Dynamix72 <84641372+Dynamix72@users.noreply.github.com>
2026-07-05 12:51:59 +0200

    Refactor config flow for ANWB Charging integration

--- custom_components/anwb_charging/config_flow.py
+++ custom_components/anwb_charging/config_flow.py
@@ -3,43 +3,37 @@ from __future__ import annotations
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.selector import (
     EntitySelector,
     EntitySelectorConfig,
     NumberSelector,
     NumberSelectorConfig,
     NumberSelectorMode,
-    TextSelector
 )
 
 from .const import (
     DOMAIN,
-    CONF_API_KEY,
     CONF_DEVICE_TRACKER,
     CONF_RADIUS,
     DEFAULT_RADIUS,
 )
 
 
-class AnwbChargingConfigFlow(
+class ConfigFlow(
     config_entries.ConfigFlow,
     domain=DOMAIN,
 ):
-    """ANWB Charging config flow."""
-
     VERSION = 1
 
     async def async_step_user(
         self,
         user_input=None,
     ):
-        errors = {}
 
         if user_input is not None:
 
             await self.async_set_unique_id(
-                f"anwb_charging_{user_input[CONF_DEVICE_TRACKER]}"
+                user_input[CONF_DEVICE_TRACKER]
             )
 
             self._abort_if_unique_id_configured()
@@ -49,40 +43,28 @@ class AnwbChargingConfigFlow(
                 data=user_input,
             )
 
-        registry = er.async_get(self.hass)
-
-        device_trackers = [
-            entity_id
-            for entity_id in registry.entities
-            if entity_id.startswith("device_tracker.")
-        ]
-
-        data_schema = vol.Schema(
-            {
-                vol.Required(
-                    CONF_DEVICE_TRACKER
-                ): EntitySelector(
-                    EntitySelectorConfig(
-                        domain="device_tracker"
-                    )
-                ),
-
-                vol.Required(
-                    CONF_RADIUS,
-                    default=DEFAULT_RADIUS,
-                ): NumberSelector(
-                    NumberSelectorConfig(
-                        min=1,
-                        max=100,
-                        step=1,
-                        mode=NumberSelectorMode.BOX,
-                    )
-                ),
-            }
-        )
-
         return self.async_show_form(
             step_id="user",
-            data_schema=data_schema,
-            errors=errors,
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_DEVICE_TRACKER
+                    ): EntitySelector(
+                        EntitySelectorConfig(
+                            domain="device_tracker"
+                        )
+                    ),
+                    vol.Required(
+                        CONF_RADIUS,
+                        default=DEFAULT_RADIUS,
+                    ): NumberSelector(
+                        NumberSelectorConfig(
+                            min=1,
+                            max=100,
+                            step=1,
+                            mode=NumberSelectorMode.BOX,
+                        )
+                    ),
+                }
+            ),
         )
