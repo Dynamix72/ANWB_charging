8b12f9fe95a5cb309fdc71bd8686c645d6b2ff5c
Dynamix72 <84641372+Dynamix72@users.noreply.github.com>
2026-07-05 12:14:24 +0200

    Update sensor.py

--- custom_components/anwb_charging/sensor.py
+++ custom_components/anwb_charging/sensor.py
@@ -1,1 +1,100 @@
+from homeassistant.components.sensor import SensorEntity
 
+from .coordinator import AnwbCoordinator
+
+
+async def async_setup_entry(
+    hass,
+    entry,
+    async_add_entities,
+):
+
+    coordinator = AnwbCoordinator(
+        hass,
+        entry.data["api_key"],
+        entry.data["device_tracker"],
+        entry.data["radius"],
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    async_add_entities(
+        [AnwbCheapestSensor(coordinator)],
+        True
+    )
+
+
+class AnwbCheapestSensor(
+    SensorEntity
+):
+
+    def __init__(self, coordinator):
+
+        self.coordinator = coordinator
+
+    @property
+    def name(self):
+
+        return "ANWB Cheapest Charger"
+
+    @property
+    def unique_id(self):
+
+        return "anwb_cheapest"
+
+    @property
+    def state(self):
+
+        chargers = (
+            self.coordinator.data["value"]
+        )
+
+        cheapest = min(
+            chargers,
+            key=lambda x: x["price"]["price"]
+        )
+
+        return cheapest["title"]
+
+    @property
+    def extra_state_attributes(self):
+
+        chargers = (
+            self.coordinator.data["value"]
+        )
+
+        cheapest = min(
+            chargers,
+            key=lambda x: x["price"]["price"]
+        )
+
+        return {
+            "price_per_kwh":
+                cheapest["price"]["price"],
+
+            "currency":
+                cheapest["price"]["currency"],
+
+            "street":
+                cheapest["address"]["streetAddress"],
+
+            "postal_code":
+                cheapest["address"]["postalCode"],
+
+            "city":
+                cheapest["address"]["city"],
+
+            "latitude":
+                cheapest["coordinates"]["latitude"],
+
+            "longitude":
+                cheapest["coordinates"]["longitude"],
+
+            "availability":
+                cheapest[
+                    "electricVehicleSupplyEquipment"
+                ],
+
+            "raw":
+                cheapest
+        }
