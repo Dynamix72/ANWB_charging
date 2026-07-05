8ef8c3d89b9e5fa149c4c2e783889faeedeb3c6a
Dynamix72 <84641372+Dynamix72@users.noreply.github.com>
2026-07-05 12:12:55 +0200

    Update const.py

--- custom_components/anwb_charging/const.py
+++ custom_components/anwb_charging/const.py
@@ -1,1 +1,9 @@
+DOMAIN = "anwb_charging"
 
+CONF_API_KEY = "api_key"
+CONF_DEVICE_TRACKER = "device_tracker"
+CONF_RADIUS = "radius"
+
+DEFAULT_RADIUS = 10
+
+API_URL = "https://api.anwb.nl/routing/points-of-interest/v3/all"
