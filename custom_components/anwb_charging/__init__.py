24cad1be29f5a554ec6ebc1adc59e6a2b45cc69a
Dynamix72 <84641372+Dynamix72@users.noreply.github.com>
2026-07-05 12:13:17 +0200

    Update __init__.py

--- custom_components/anwb_charging/__init__.py
+++ custom_components/anwb_charging/__init__.py
@@ -1,1 +1,16 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+PLATFORMS = ["sensor"]
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry
+) -> bool:
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS
+    )
+
+    return True
