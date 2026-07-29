54f7e9fda5e853fdbfb4b5d6896b29e395e2f5c3
Dynamix72 <84641372+Dynamix72@users.noreply.github.com>
2026-07-29 14:45:12 +0200

    Add configuration constants and update default radius

--- custom_components/anwb_charging/const.py
+++ custom_components/anwb_charging/const.py
@@ -3,4 +3,10 @@ DOMAIN = "anwb_charging"
 CONF_DEVICE_TRACKER = "device_tracker"
 CONF_RADIUS = "radius"
 
-DEFAULT_RADIUS = 10
+CONF_MIN_POWER_KW = "min_power_kw"
+CONF_MAX_DETOUR_KM = "max_detour_km"
+CONF_ORS_API_KEY = "ors_api_key"
+
+DEFAULT_RADIUS = 25
+DEFAULT_MIN_POWER_KW = 100
+DEFAULT_MAX_DETOUR_KM = 10
